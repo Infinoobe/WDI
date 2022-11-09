fe6055978f68782c50f422c04c91af6e0ceef359
Infinoobe <35398521+Infinoobe@users.noreply.github.com>
2022-11-09 09:33:48 +0100

    Update zad2.7.c

--- zad2.7.c
+++ zad2.7.c
@@ -1,19 +1,25 @@
 #include <stdio.h>
 #include <math.h>
-int main() {
-    int n, pomocnicza=0, i=2;
-    printf("Podaj liczbe:");
-    scanf("%d", &n);
-    for(i;i<sqrt(n);i++){
+int c_p(int n, int i=2){
+for(i;i<sqrt(n);i++){
         if(n%i==0){
             pomocnicza++;
         }
     }
-    if(pomocnicza==0){
+    if(pomocnicza==0 && n!=1){
         printf("liczba jest pierwsza");
+        return 1;
     }
     else{
         printf("liczba nie jest pierwsza");
+        return 0;
     }
-    return 0;
+    
+}
+
+int main() {
+    int n, pomocnicza=0, i=2;
+    printf("Podaj liczbe:");
+    scanf("%d", &n);
+    printf("%d", c_p(n));
 }
